2f9d1a4f902c17274dd7e7deab9b1b244f473a2b
Sai Krishna Vallabhaneni <sai9@mo.ece.pdx.edu>
2023-02-02 22:19:39 -0800

    Lab3

--- lec/scripts/lec_rtl2dc.tcl
+++ lec/scripts/lec_rtl2dc.tcl
@@ -6,10 +6,8 @@
 # lec -tclmode -dofile ../scripts/lec_rtl2dc.tcl
 
 global env
-read_design ../../syn/rtl/$env(TOP2).sv -Systemverilog -Golden -sensitive -continuousassignment Bidirectional -nokeep_unreach   -nosupply -root $env(TOP2)
-read_design ../../syn/rtl/$env(TOP1).sv -Systemverilog -REVised -sensitive -continuousassignment Bidirectional -nokeep_unreach   -nosupply -root $env(TOP1)
-
-#read_design [list /pkgs/synopsys/2020/32_28nm/SAED32_EDK/lib/stdcell_rvt/verilog/saed32nm.v ../../syn/outputs/$env(TOP).dc.vg ] -Systemverilog  -revised -sensitive -continuousassignment Bidirectional -nokeep_unreach   -nosupply -root $env(TOP)
+read_design ../../syn/rtl/$env(TOP).sv -Systemverilog -Golden -sensitive -continuousassignment Bidirectional -nokeep_unreach   -nosupply -root $env(TOP)
+read_design [list /pkgs/synopsys/2020/32_28nm/SAED32_EDK/lib/stdcell_rvt/verilog/saed32nm.v ../../syn/outputs/$env(TOP).genus.vg ] -Systemverilog  -revised -sensitive -continuousassignment Bidirectional -nokeep_unreach   -nosupply -root $env(TOP)
 set_system mode lec
 add_compared_points -all
 compare
